b2a0e1e952f662ffe062e1bcb68973a0b26e47a6
rfarley <rfarley@mitre.org>
2016-01-13 17:08:15 -0500

    Decoder done

--- bash_decode.py
+++ bash_decode.py
@@ -7,9 +7,16 @@ import base64
 for line in sys.stdin:
     # print(line)
     data = json.loads(line)
-    print(data)
+    # print(data)
     decoded = 'invalid or none command'
     if 'cmd' in data and data['cmd'] is not None:
         decoded = base64.b64decode(data['cmd'])
     data['cmd'] = decoded
-    print('%s' % json.dumps(data))
+    sess = 'no_session'
+    if 'src' in data and 'session' in data['src']:
+        sess = data['src']['session']
+    user = 'unk_user'
+    if 'src' in data and 'user' in data['src']:
+        user = data['src']['user']
+    # print('%s' % json.dumps(data))
+    print('%s %s:%s %s %s' % (int(data['server_ts']), data['src']['remote_ip'], user, sess, data['cmd']))
