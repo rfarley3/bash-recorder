b9e4eab4ab8923fe9e08137051c038c0307991ff
rfarley <rfarley@mitre.org>
2016-01-13 23:05:37 -0500

    POC remote logger for generic cmds from python programs

---
+++ remote_log_fn.py
@@ -0,0 +1,11 @@
+
+def remote_log(msg, user=None):
+    data = {}
+    data['cmd'] = msg
+    data['src'] = {}
+    data['src']['ctf_user'] = user
+    url = 'https://ctf.local:9999/cmd'
+    try:
+        resp = requests.post(url, data=json.dumps(data), timeout=2.0)
+    except:  # ConnectionError, HTTPError, Timeout, TooManyRedirects
+        pass
